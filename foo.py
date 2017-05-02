9b2e7b8615d7811c24e1622bfdeb00e1a52ffa55
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-05-02 22:30:15 +0530

    updating records'

--- foo.py
+++ foo.py
@@ -5,9 +5,10 @@ import pprint
 import collections
 
 listoffiles = os.listdir("./Data/Fights")
+errors = set()
 
 def flatten(d, parent_key='', sep='_'):
-    print "in flatten"
+    print d
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
@@ -22,50 +23,76 @@ def create_dict(indict):
   return {
       'Event_ID': indict['FMLiveFeed']['EventID'],
       'Fight_ID': indict['FMLiveFeed']['FightID'],
-      'Blue_Fighter_ID': indict['FMLiveFeed']['Fighters']['Blue']['FighterID'],
-      'Blue_Name': indict['FMLiveFeed']['Fighters']['Blue']['Name'],
-      'Red_Fighter_ID': indict['FMLiveFeed']['Fighters']['Red']['FighterID'],
-      'Red_Name': indict['FMLiveFeed']['Fighters']['Red']['Name']
+      'B_ID': indict['FMLiveFeed']['Fighters']['Blue']['FighterID'],
+      'B_Name': indict['FMLiveFeed']['Fighters']['Blue']['Name'],
+      'R_ID': indict['FMLiveFeed']['Fighters']['Red']['FighterID'],
+      'R_Name': indict['FMLiveFeed']['Fighters']['Red']['Name'],
+      'Date': indict['Timestamp'].split(' ')[1]
   }
 
 
 def readfile(inputstr, header,flag=False):
   print "in readFile"
-  with open(header + inputstr) as datafile:
-    herp = json.load(datafile)
-    if(flag):
-      event_fight = inputstr.split('_')
-      herp['Event_id'] = event_fight[0]
-      herp['Fight_id'] = event_fight[1].split('.')[0]
-      boobs = herp['Fighter']
-    return herp
-
+  try:
+    with open(header + inputstr) as datafile:
+      herp = json.load(datafile)
+      if(flag==True):
+        event_fight = inputstr.split('_')
+        herp['Event_id'] = event_fight[0]
+        herp['Fight_id'] = event_fight[1].split('.')[0]
+        boobs = herp['Fighter']
+      return herp
+  except:
+    errors.add(header+inputstr)
+    return
 
 def EditDict(indict):
   print "in EditDict"
-  bluefighterstring = str(indict['Event_ID'])+str('_')+str(indict['Fight_ID'])+str('_')+ str(indict['Blue_Fighter_ID'])+'.json'
-  redfighterstring = str(indict['Event_ID'])+str('_')+str(indict['Fight_ID'])+str('_')+ str(indict['Red_Fighter_ID'])+'.json'
+  bluefighterstring = str(indict['Event_ID'])+str('_')+str(indict['Fight_ID'])+str('_')+ str(indict['B_ID'])+'.json'
+  redfighterstring = str(indict['Event_ID'])+str('_')+str(indict['Fight_ID'])+str('_')+ str(indict['R_ID'])+'.json'
   blue_fighter_dict = readfile(bluefighterstring,'./Data/Fights_Fighter/')
+  
+  
+  blue_fighter_statstring = str(indict['B_Name'])+'.json'
+  blue_fighter_statdict = readfile(blue_fighter_statstring,'./Data/fighters/')
+  if(blue_fighter_statdict):
+    blue_fighter_dict['B_Weight'] = blue_fighter_statdict['weight_kg']
+    blue_fighter_dict['B_Height'] = blue_fighter_statdict['height_cm']
+    blue_fighter_dict['B_HomeTown'] = blue_fighter_statdict['hometown']
+    blue_fighter_dict['B_Location'] = blue_fighter_statdict['location']
+    blue_fighter_dict['B_Age'] = blue_fighter_statdict['age']
+  
+  red_fighter_statstring = str(indict['R_Name'])+'.json'
   red_fighter_dict = readfile(redfighterstring,'./Data/Fights_Fighter/')
+  red_fighter_statdict = readfile(red_fighter_statstring,'./Data/fighters/')
+  if(red_fighter_statdict):
+    red_fighter_dict['R_Weight'] = red_fighter_statdict['weight_kg']
+    red_fighter_dict['R_Height'] = red_fighter_statdict['height_cm']
+    red_fighter_dict['R_HomeTown'] = red_fighter_statdict['hometown']
+    red_fighter_dict['R_Location'] = red_fighter_statdict['location']
+    red_fighter_dict['R_Age'] = red_fighter_statdict['age']
+  
   val = blue_fighter_dict['Fighter_stats']
-  blue_fighter_dict['Blue_Fighter_stats'] = val
+  blue_fighter_dict['B_'] = val
   val = blue_fighter_dict['Record']
-  blue_fighter_dict['BlueRecord'] = val
+  blue_fighter_dict['BRecord'] = val
   blue_fighter_dict.pop('Fighter',None)
   blue_fighter_dict.pop('Fighter_stats',None)
   blue_fighter_dict.pop('Record',None)
   indict.update(blue_fighter_dict)
+  
   val = red_fighter_dict['Fighter_stats']
-  red_fighter_dict['Red_Fighter_stats'] = val
+  red_fighter_dict['R_'] = val
   val = red_fighter_dict['Record']
-  red_fighter_dict['RedRecord'] = val
+  red_fighter_dict['RRecord'] = val
   red_fighter_dict.pop('Record',None)
   red_fighter_dict.pop('Fighter',None)
   red_fighter_dict.pop('Fighter_stats',None)
   indict.update(red_fighter_dict)
-  if (winners[str(indict['Event_ID'])+'_'+str(indict['Fight_ID'])] == indict['Blue_Fighter_ID']):
+  
+  if (winners[str(indict['Event_ID'])+'_'+str(indict['Fight_ID'])] == indict['B_ID']):
     indict['winner'] = 'blue'
-  elif (winners[str(indict['Event_ID'])+'_'+str(indict['Fight_ID'])] == indict['Red_Fighter_ID']):
+  if (winners[str(indict['Event_ID'])+'_'+str(indict['Fight_ID'])] == indict['R_ID']):
     indict['winner'] = 'red'
   elif(winners[str(indict['Event_ID'])+'_'+str(indict['Fight_ID'])]== '11111'):
     indict['winner'] = 'draw'
@@ -87,4 +114,6 @@ updatedDicts = map(EditDict,FighterAndEventDic)
 flatteneddicts = map(flatten,updatedDicts)
 pp.pprint(flatteneddicts[-2])
 df = pd.DataFrame.from_records(flatteneddicts)
-df.to_csv('./Data/finalout.csv')
+df.to_csv('./Data/finalout.csv',index=False, encoding='utf-8')
+for x in errors:
+  print x
